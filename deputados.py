00ae02d9050a1727ce7f924258a2dcacbab370ce
sablewin22 <slewin2202@gmail.com>
2026-05-05 15:14:32 -0300

    Update deputados.py

--- deputados.py
+++ deputados.py
@@ -8,11 +8,11 @@ partidos = st.text_input('Digite o partido que você queira ver os deputados:')
 uf = st.text_input('Digite o UF dos deputados que você quer ver:')
 
 if partidos:
-    df_filtrado = df[df['partido'] == partidos.upper()])
+    df_filtrado = df[df['partido'] == partidos.upper()]
 else:
     df_filtrado = df
 
 if uf:
-    df_filtrado = df_filtrado[df_filtrado['uf'] == uf.upper()])
+    df_filtrado = df_filtrado[df_filtrado['uf'] == uf.upper()]
 
 st.dataframe(df_filtrado)
