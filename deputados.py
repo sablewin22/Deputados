b795a7fa5758a519e22e195d7036b2652ce8a8ef
sablewin22 <slewin2202@gmail.com>
2026-05-05 14:50:45 -0300

    Update deputados.py

--- deputados.py
+++ deputados.py
@@ -3,11 +3,7 @@ import pandas as pd
 df = pd.read_csv('deputados_2022.csv')
 st.dataframe(df)
 
-opcao = st.text_input(
-    'Digite o partido que deseja ver os deputados que fazem parte:'
-)
-if opcao:
-    filtrado = df[df['partido'].str.contains(opcao, case=False, na=False)]
-    
-    st.write(f'Deputados do partido "{opcao}":')
-    st.dataframe(filtrado)
+partido = st.text_input('Digite o partido:')
+
+if partido:
+    st.dataframe(df[df['partido'] == partido])
