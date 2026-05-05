1e0ad5b741f0e160f3cb6ca5b003591a38c043ef
sablewin22 <slewin2202@gmail.com>
2026-05-05 14:46:13 -0300

    Update deputados.py

--- deputados.py
+++ deputados.py
@@ -3,3 +3,6 @@ import pandas as pd
 df = pd.read_csv('deputados_2022.csv')
 st.dataframe(df)
 
+opcao = selectbox(
+  'Escolha o partido que deseja ver os deputados que fazemn parte',
+  ['deputados_2022.csv(partido)']
