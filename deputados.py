fa7f9d469d32399ffeb3873c660fa515983a2907
sablewin22 <slewin2202@gmail.com>
2026-05-05 15:00:27 -0300

    Update deputados.py

--- deputados.py
+++ deputados.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd 
 df = pd.read_csv('deputados_2022.csv')
-st.dataframe(df)
 
 partidos = st.text_input('Digite o partido que você queira ver os deputados:')
 
